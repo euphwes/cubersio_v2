019ab6e2cd2ff0e66a6e20255bc1fc78d1d5785c
Wes Evans <euphwes@gmail.com>
2025-01-31 15:03:31 -0700

    added images for all events, starting styling point

--- frontend/src/lib/EventCard.svelte
+++ frontend/src/lib/EventCard.svelte
@@ -2,7 +2,7 @@
 @component
 
 EventCard
-
+7CB278
 TODO
 -->
 
@@ -10,151 +10,64 @@ TODO
   const { eventSlug } = $props();
 </script>
 
-<div class="event-card drop-shadow ">
-    <div class="event-image-container">
-        <img class="event-image" src="/images/cube-{eventSlug}.png">
-        <div class="overlay complete-overlay">
-            <a href="#" class="icon"><i class="fas fa-check"></i></a>
-        </div>
-        <div class="overlay incomplete-overlay">
-            <a href="#" class="icon"><i class="far fa-clock"></i></a>
-        </div>
-    </div>
-    <div class="event-name">
-        <hr>
-        <div class="row">
-            <div class="col-12"><span class="event-title">{eventSlug}</div>
-        </div>
-    </div>
-    <span class="event-summary">Summary</span>
-    <div class="bonus-event-indicator"><i class="fas fa-gift"></i></div>
+<div class="event-card drop-shadow">
+  <div class="event-image-container">
+    <img class="event-image" src="/images/{eventSlug}.png" alt="{eventSlug} image" />
+  </div>
+  <hr />
+  <div class="event-title">{eventSlug}</div>
+  <span class="event-summary">10.25</span>
 </div>
 
 <style>
-
-
-.event-card {
+  .event-card {
     text-align: center;
     background-color: white;
-
-    margin: 15px;
-    padding: 15px;
-
-    border-radius: 8px;
-    
-    -webkit-transition: all 250ms ease;
-    -moz-transition: all 250ms ease;
-    -o-transition: all 250ms ease;
+    margin: 1rem;
+    padding: 1rem;
+    border-radius: 0.5rem;
     transition: all 250ms ease;
-
     position: relative;
+  }
 
-    &.complete {
-        .icon {
-            color: desaturate(#3A8AAF, 15%);
-        }
-        .complete-overlay {
-            opacity: 1;
-            transition: all 250ms ease;
-        }
-    }
+  .event-card:hover {
+    transform: translate(0, -0.625rem);
+    transition: all 250ms ease;
+  }
 
-    &.incomplete {
-        .icon {
-            color: desaturate(#3A8AAF, 15%);
-        }
-        .incomplete-overlay {
-            opacity: 1;
-            transition: all 250ms ease;
-        }
-    }
+  .event-card:hover .event-image {
+    /* filter: saturate(75%);
+    -webkit-filter: saturate(75%); */
+  }
 
-    &.complete, &.incomplete {
-        &:hover {
-            .overlay {
-                opacity: 0;
-                transition: all 250ms ease;
-            }
-        }
-    }
+  .event-image-container {
+    margin-bottom: 1rem;
+  }
 
-    &:hover {
-        transform: translate(0px, -10px);
+  .event-title {
+    margin-top: 0.5rem;
+  }
 
-        -webkit-transition: all 250ms ease;
-        -moz-transition: all 250ms ease;
-        -o-transition: all 250ms ease;
-        transition: all 250ms ease;
-
-        .event-image {
-            -webkit-filter: saturate(75%);
-            filter: saturate(75%);
-        }
-    }
-}
-
-.event-image-container {
-
-position: relative;
-height: 110px;
-width: 110px;
-
-}
-
-.event-image {
-    -webkit-filter: saturate(0%) brightness(1.40);
-    filter: saturate(0%) brightness(1.40);
+  .event-image {
+    /* -webkit-filter: saturate(0%) brightness(1.4); */
+    /* filter: saturate(0%) brightness(1.4); */
 
     -webkit-transition: filter 250ms ease;
     -moz-transition: filter 250ms ease;
     -o-transition: filter 250ms ease;
     transition: filter 250ms ease;
 
-    width: 100%;
-    height: 100%;
-}
-
-.overlay {
-position: absolute;
-top: 0; bottom: 0; left: 0; right: 0;
-opacity: 0;
-transition: all 250ms ease;
-
-
-}
-
-.icon {
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    text-align: center;
-    position: absolute;
+    /* width: 100%; */
+    /* height: 100%; */
+    width: 120px;
+    height: 120px;
   }
 
-
-    span.event-title {
-        font-size: 14px;
-        font-family: 'Roboto', sans-serif;
-    }
-
-    span.event-summary {
-        font-size: 12px;
-        opacity: 0.66;
-        position: absolute;
-        right: 5px;
-        bottom: 5px;
-    }
-
-.event-cards {
-    justify-content: center!important;
-}
-
-.bonus-event-indicator {
-    position: absolute;
-    left: 5px;
-    bottom: 5px;
-    color: #9bd0db;
-    font-size: 14px;
-}
+  .event-summary {
+    font-size: 0.75rem;
+    opacity: 0.66;
+    position: fixed;
+    right: 0.25rem;
+    bottom: 0.25rem;
+  }
 </style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,10 +6,9 @@
 </script>
 
 <div class="app-container">
-  <div style="justify-content: center!important; display: flex;">
+  <div style="display: flex; flex-wrap: wrap;">
     {#each data.events as event}
-    <EventCard eventSlug={event} />
-  {/each}
+      <EventCard eventSlug={event} />
+    {/each}
   </div>
-
 </div>
