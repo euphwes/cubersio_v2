db9c7c90b090f866a187bd88c47cbadce21d9326
Wes Evans <euphwes@gmail.com>
2025-02-16 13:15:59 -0700

    wip 10x10

--- backend/cubersio/svg_gen.py
+++ backend/cubersio/svg_gen.py
@@ -1,9 +1,9 @@
 from random import choice
 
-for y in [0, 29, 58, 87, 116, 145, 174]:
-    for x in [0, 29, 58, 87, 116, 145, 174]:
+for y in [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]:
+    for x in [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]:
         c = choice(["white", "red", "blue", "green", "yellow", "orange"])
         print(
-            f'<rect x="{x}" y="{y}" width="26" height="26" rx="6" ry="6" fill="var(--color-{c})" />'
+            f'<rect x="{x}" y="{y}" width="17" height="17" rx="4" ry="4" fill="var(--color-{c})" />'
         )
     print()
